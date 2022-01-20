b77f64b9e3bdc2394c9b092e3fa0b283c036c045
EladBarkay <eladb1231@gmail.com>
2022-01-20 18:00:21 +0200

    Exception for non matching records

---
+++ src/main/java/RecordsNotMatchException.java
@@ -0,0 +1,8 @@
+public class RecordsNotMatchException extends Exception{
+    public RecordsNotMatchException(String msg) {
+        super(msg);
+    }
+
+    public RecordsNotMatchException() {
+    }
+}
